a16bd58236e6b747ff8212e2aea3cb9d409750fe
spwahaha <spwahaha@gmail.com>
2016-08-26 12:31:18 -0400

    update gitignore and add one problem

---
+++ Coding Problems/src/leetcode/Valid_Perfect_Square.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Valid_Perfect_Square {
+    public static boolean isPerfectSquare(int num) {
+        if(num < 0) return false;
+        if(num == 0) return true;
+        int l = 1;
+        int h = num;
+        while(l <= h){
+            long mid = l + (h - l) / 2;
+            long square = mid * mid;
+            if(square == num) return true;
+            if(square < num) l = (int)mid + 1;
+            if(square > num) h = (int)mid - 1;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args){
+    	System.out.println(isPerfectSquare(Integer.MAX_VALUE));
+    }
+}
